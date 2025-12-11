4b7ec4ef96ab6ac349ee8f3fd0e589f7fcfde4f5
Tarun Mandal <tarunmandal@bakhultech.com>
2025-12-11 15:21:33 +0530

    Update

--- routes/ContactRoute.js
+++ routes/ContactRoute.js
@@ -4,7 +4,6 @@ const ContactController = require("../controllers/ContactController");
 const multer = require("multer");
 const upload = multer();
 
-// Use POST for both endpoints
 router.post("/get_contacts", ContactController.getAllContacts);
 router.post("/add_contact", upload.none(), ContactController.addContact);
 
