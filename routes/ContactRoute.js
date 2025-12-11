52cf2b83148fc272296ad5e657c2548a690fa10c
Tarun Mandal <tarunmandal@bakhultech.com>
2025-12-11 15:34:22 +0530

    Update_contact_name

--- routes/ContactRoute.js
+++ routes/ContactRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const ContactController = require("../controllers/ContactController");
+const ContactController = require("../controllers/ContactControllerr");
 const multer = require("multer");
 const upload = multer();
 
